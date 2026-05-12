e9940d30bdbede3e0b8fba6f99ed8391a91fb0a8
NanoDragon1234 <adrianherrero2016@gmail.com>
2026-05-12 20:57:37 +0200

    Fix: commit trailers. etc

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,13 +1,13 @@
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import {Router} from '@angular/router';
 
   const authService = inject(AuthService);
   const router = inject(Router);
 
   if (authService.isLoggedIn()) {
+    // @ts-ignore
     return true;
   }
 
   router.navigate(['/login']);
-  return false;
-};
